16b46e0e746bc68a499e0963f71e0933a7bcb49d
Victor Akhigbemidu <vehiz1520@gmail.com>
2023-02-07 00:48:55 +0100

    test added

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
 
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5001
+const app = express();
+const PORT = process.env.PORT || 5001;
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 // Registering routes
-app.get("/", (req, res)=>{
-    res.status(200).send("Weather API is running")
-})
+app.get("/", (req, res) => {
+    res.status(200).send("Weather API is Running!");
+  });  
 
 // Fetching weather forecast for a particular city
-app.get("/weather", async (req, res)=>{
-    if(!req.query.city){
-        res.status(404).json("City is missing")
-    }else{
-        let city = req.query.city;
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-        const data = await response.json()
-        res.status(200).json(data)
-    }
-} )
+app.get("/weather", async (req, res) => {
+  if (!req.query.city) {
+    res.status(404).json("City is missing");
+  } else {
+    let city = req.query.city;
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
+    );
 
-app.listen(PORT, ()=>{
-    console.log(`server running on ${PORT}`)
-})
+    const data = await response.json();
+    res.status(200).json(data);
+  }
+});
 
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
