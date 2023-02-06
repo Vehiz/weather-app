616ed459802e42689f8b1c4779414f5564d6ed96
Victor Akhigbemidu <vehiz1520@gmail.com>
2023-02-06 17:17:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 5001
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Registering routes
+app.get("/", (req, res)=>{
+    res.status(200).send("Weather API is running")
+})
+
+// Fetching weather forecast for a particular city
+app.get("/weather", async (req, res)=>{
+    if(!req.query.city){
+        res.status(404).json("City is missing")
+    }else{
+        let city = req.query.city;
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
+        const data = await response.json()
+        res.status(200).json(data)
+    }
+} )
+
+app.listen(PORT, ()=>{
+    console.log(`server running on ${PORT}`)
+})
+
